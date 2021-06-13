27b66ae1b11a8c9ae32fdf8f47d260f2679f9a3f
Aimal Khan <Aimal.khan@bryanuniversity.edu>
2021-06-13 00:31:30 -0400

    week-1

---
+++ week-1/Math/app.js
@@ -0,0 +1,12 @@
+//addition
+const mathOb = require('./math').default;
+
+console.log(mathOb.sum(33,2));
+//subtraction
+
+console.log(mathOb.subtract(15,2));
+//multi
+
+console.log(mathOb.multi(5,2));
+//divide
+console.log(mathOb.divide(20,2));
---
+++ week-1/Math/math.js
@@ -0,0 +1,18 @@
+const sum = (a,b) => {
+    return a + b
+}
+const subtract = (c,d) => {
+    return c-d
+}
+const multi = (e,f) => {
+    return e*f
+}
+const divide = (g,h) => {
+    return g/h
+}
+export default {
+    sum,
+    subtract,
+    multi,
+    divide
+};
